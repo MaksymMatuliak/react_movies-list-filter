c0e6bb4de35c87193f035862ba2cc9590bb5fe03
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-05-13 21:41:28 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.95917588f26a6ef52e875501f65f3813.js"
 );
 
 self.addEventListener('message', (event) => {
